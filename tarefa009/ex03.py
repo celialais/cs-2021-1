7ef2165ba05ee8625dde85a66e54226c8db8a92e
Célia Laís Rodrigues <celialaisrd@gmail.com>
2021-08-20 21:21:00 -0300

    tarefa009

---
+++ tarefa009/ex03.py
@@ -0,0 +1,4 @@
+saldo = float(input("Qual o valor do saldo: ")) 
+reajuste = saldo + (saldo *0.15)
+
+print ("O valor do saldo com reajuste de 15% é {}".format(reajuste))
---
+++ tarefa009/ex06.py
@@ -0,0 +1,3 @@
+numero = int(input ("Informe um número inteiro: "))
+
+print("O antecessor do número informado é {} e o sucessor é {}".format(numero-1,numero+1))
